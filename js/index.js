3bec2a14c514dc596bad6962ff6c23658ba17693
blingnj <86402349+blingnj@users.noreply.github.com>
2021-08-26 14:13:36 +0900

    0826-0410

--- js/index.js
+++ js/index.js
@@ -1,67 +1,74 @@
-//---------- 공지사항 엑스버튼 -------- 누르면 사라짐----------------------------
-        const headClose = document.querySelector('.notice'),
-              xBtn = document.querySelector('.close_btn');
+const headClose = document.querySelector('.notice');
+const xBtn = document.querySelector('.close_btn');
+const openBtn = document.querySelector('.menu-open');
+const closeBtn = document.querySelector('.menu-close');
+const navToggle = document.querySelector('.navigation');
+const actBody = document.querySelector('body');
+const btnLanguage = document.querySelector('.btn_lang');
+const language = document.querySelector('.language');
+const langIcon = document.querySelector('.btn_lang img');
+const depth= document.querySelectorAll('.depth1');
+const navul= document.querySelector('.mainnav');
+const navli= document.querySelectorAll('.mainnav >li');
+const navBack = document.querySelector('header');
+const navhead = document.querySelector('.header')
+const navheadaft = document.querySelector('header::after')
+const direction = document.querySelectorAll('.direction')
+const reserveFix = document.querySelector('.reserve_fix')
+const reserveSub = document.querySelectorAll('.sub-reserve')
+const elTop = document.querySelector('.topbtn');
+const elCon = document.querySelector('main');
+const elHeader = document.querySelector('header').offsetHeight;
 
-        xBtn.addEventListener('click',function(){
-            headClose.classList.add('active')
-        })
+
+
+//---------- 공지사항 엑스버튼 -------- 누르면 사라짐----------------------------
+
+    xBtn.addEventListener('click',function(){
+        headClose.classList.add('active')
+    })
 
 //---------- 헤더 LANGUAGE ---------- 언어변환 --------------------------
-        const btnLanguage = document.querySelector('.btn_lang')
-        const language = document.querySelector('.language')
-        const langIcon = document.querySelector('.btn_lang img')
 
-        btnLanguage.addEventListener('click',function(){
-            language.classList.toggle('active')
+    btnLanguage.addEventListener('click',function(){
+        language.classList.toggle('active')
 
-            if(language.classList[1] == 'active'){
-            langIcon.style = `transform:rotate(180deg);`
-            }else{
-            langIcon.style = `transform:rotate(0deg);`
-            }
-        })
+        if(language.classList[1] == 'active'){
+        langIcon.style = `transform:rotate(180deg);`
+        }else{
+        langIcon.style = `transform:rotate(0deg);`
+        }
+    })
 // ---------- 네비에 마우스오버 ---------- 하단메뉴나오게 --------------------------
-        const depth= document.querySelectorAll('.depth1');
-        const navul= document.querySelector('.mainnav');
-        const navli= document.querySelectorAll('.mainnav >li');
-        const navBack = document.querySelector('header');
-        const navhead = document.querySelector('.header')
-        const navheadaft = document.querySelector('header::after')
-        const direction = document.querySelectorAll('.direction')
 
+    let idx=0;
+    for(let i=0; i<navli.length; i++){
+        navli[i].addEventListener('click',function(){
+            if(depth[i].classList.contains('open')){
+                depth[i].classList.remove('open')
+                direction[i].classList.remove('open')
+            }else{
+                depth[idx].classList.remove('open')
+                direction[idx].classList.remove('open')
+                depth[i].classList.add('open')
+                direction[i].classList.add('open')
+            }
+            idx=i;
+        })
+    }
 
-        let idx=0;
-        for(let i=0; i<navli.length; i++){
-            navli[i].addEventListener('click',function(){
-                if(depth[i].classList.contains('open')){
-                    depth[i].classList.remove('open')
-                    direction[i].classList.remove('open')
-                }else{
-                    depth[idx].classList.remove('open')
-                    direction[idx].classList.remove('open')
-                    depth[i].classList.add('open')
-                    direction[i].classList.add('open')
-                }
-                idx=i;
+    function mouseon(){
+        for(let k=0; k<depth.length; k++){ 
+            navul.addEventListener('mouseenter',function(){
+                depth[k].classList.add('active');
+                navBack.classList.add('active');
+            })
+            navhead.addEventListener('mouseleave',function(){
+                depth[k].classList.remove('active');
+                navBack.classList.remove('active');
             })
         }
-
-        // if(window.matchMedia("(min-width:1120px)").matches){
-            
-        // }
-
-        function mouseon(){
-            for(let k=0; k<depth.length; k++){ 
-                navul.addEventListener('mouseenter',function(){
-                    depth[k].classList.add('active');
-                    navBack.classList.add('active');
-                })
-                navhead.addEventListener('mouseleave',function(){
-                    depth[k].classList.remove('active');
-                    navBack.classList.remove('active');
-                })
-            }
-        }mouseon();
+    }mouseon();
         
 
 //******************** 슬라이더 이미지 가져오기 *************************//
@@ -86,33 +93,7 @@ function callback(data){
     slickFun();
 }
 
-function slickFun(){
-    $(".regular").slick({
-    arrows:true,//false 좌우버튼이 없어짐
-    dots: false,//인디게이터가 있음 false없음 
-    infinite: true,
-    slidesToShow: 3, // 몇장을 한번에 보여주는지.
-    slidesToScroll: 1, // 1= 1장씩 움진인다 2= 2장씩 움직인다. 
-    autoplay:false,
-    
-    responsive: [
-        {
-          breakpoint: 1119,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1,
-          }
-        },
-        {
-            breakpoint: 767,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-            }
-        }
-    ]
-    });
-}
+
 //------------------------------ 스와이퍼 -----------------------------------------------------
 const swiper = new Swiper('.swiper-container', {
 
@@ -133,55 +114,50 @@ const swiper = new Swiper('.swiper-container', {
     //autoplay: {delay: 5000,   },
   });
 //------------------ 사이즈 변경시 예약버튼 active -------------------------------
-        const reserveFix = document.querySelector('.reserve_fix')
-        const reserveSub = document.querySelectorAll('.sub-reserve')
 
-        for(let i=0; i<reserveSub.length; i++){
+    for(let i=0; i<reserveSub.length; i++){
 
-            window.addEventListener('resize', function(e){
-                sizeChange();
-            });
-
-            reserveFix.addEventListener('click',function(){
-                reserveSub[i].classList.toggle('active')
-            })
-        }
-//---------------- 스크롤 탑버튼 -------------------------------------------------
-        const elTop = document.querySelector('.topbtn');
-                
-        window.addEventListener('scroll',function(){
-            if(window.innerHeight < window.scrollY){
-                elTop.classList.add('active');
-            }else{
-                elTop.classList.remove('active');
-            };
+        window.addEventListener('resize', function(e){
+            sizeChange();
         });
-        const elCon = document.querySelectorAll('main');
-        const elHeader = document.querySelector('header').offsetHeight;
 
-        elTop.addEventListener('click',function(e){
-            e.preventDefault();
-
-            window.scrollTo({
-                top:elCon.offsetTop-elHeader,
-                behavior:"smooth"
-            });
+        reserveFix.addEventListener('click',function(){
+            reserveSub[i].classList.toggle('active')
         })
-//*************************** 헤더 토글버튼 ******************************//
-const openBtn = document.querySelector('.menu-open');
-const closeBtn = document.querySelector('.menu-close');
-const navToggle = document.querySelector('.navigation');
-const actBody = document.querySelector('body')
+    }
 
-openBtn.addEventListener('click',function(){
-    navToggle.classList.add('active')
-    actBody.classList.add('active')
-})
-closeBtn.addEventListener('click',function(){
-    navToggle.classList.remove('active')
-    actBody.classList.remove('active')
-})
+//---------------- 스크롤 탑버튼 -------------------------------------------------
+          
+    window.addEventListener('scroll',function(){
+        if(window.innerHeight < window.scrollY){
+            elTop.classList.add('active');
+        }else{
+            elTop.classList.remove('active');
+        };
+    });
+
+    elTop.addEventListener('click',function(e){
+        e.preventDefault();
+
+        window.scrollTo({
+            top:elCon.offsetTop-elHeader,
+            behavior:"smooth"
+        });
+    })
+
+//*************************** 헤더 토글버튼 ******************************//
+
+    openBtn.addEventListener('click',function(){
+        navToggle.classList.add('active')
+        actBody.classList.add('active')
+    })
+    closeBtn.addEventListener('click',function(){
+        navToggle.classList.remove('active')
+        actBody.classList.remove('active')
+    })
+
 //******************** function *******************************************//
+
 function sizeChange(){
     for(let i=0; i<reserveSub.length; i++){
 
@@ -198,4 +174,30 @@ function sizeChange(){
         }
     }
 }
-sizeChange();
+
+function slickFun(){
+    $(".regular").slick({
+    arrows:true,//false 좌우버튼이 없어짐
+    dots: false,//인디게이터가 있음 false없음 
+    infinite: true,
+    slidesToShow: 3, // 몇장을 한번에 보여주는지.
+    slidesToScroll: 1, // 1= 1장씩 움진인다 2= 2장씩 움직인다. 
+    autoplay:false,
+    responsive: [
+        {
+          breakpoint: 1119,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+          }
+        },
+        {
+            breakpoint: 767,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+            }
+        }
+    ]
+    });
+}
--- js/sub.js
+++ js/sub.js
@@ -1,7 +1,16 @@
-//---------- 헤더 LANGUAGE ---------- 언어변환 --------------------------
 const btnLanguage = document.querySelector('.btn_lang')
 const language = document.querySelector('.language')
 const langIcon = document.querySelector('.btn_lang img')
+const tapBtn = document.querySelectorAll('.tapbtn')
+const tapImg = document.querySelectorAll('.tapimg')
+const reserveFix = document.querySelector('.reserve_fix')
+const reserveSub = document.querySelectorAll('.sub-reserve')
+const elTop = document.querySelector('.topbtn');
+const elmain = document.querySelector('main');
+const elHeader = document.querySelector('header').offsetHeight;
+const navul= document.querySelector('.sec_nav');
+
+//---------- 헤더 LANGUAGE ---------- 언어변환 --------------------------
 
 btnLanguage.addEventListener('click',function(){
   language.classList.toggle('active')
@@ -14,8 +23,7 @@ btnLanguage.addEventListener('click',function(){
 })
 
 //---------- 탭 ------------------------------------
-const tapBtn = document.querySelectorAll('.tapbtn')
-const tapImg = document.querySelectorAll('.tapimg')
+
 let num=0;
 for(let i=0; i<tapBtn.length; i++){
  tapBtn[i].addEventListener('click',function(){
@@ -40,36 +48,53 @@ $(".slider").slick({
     centerMode: true,
     variableWidth: true
   });
+//------------------ 사이즈 변경시 예약버튼 active -------------------------------
 
+for(let i=0; i<reserveSub.length; i++){
 
-//************************예약버튼*************************//
-const reserveFix = document.querySelector('.reserve_fix')
-const reserveSub = document.querySelectorAll('.reserve_sub')
+    window.addEventListener('resize', function(e){
+        sizeChange();
+    });
 
-
-reserveFix.addEventListener('click',function(){
-    for(let i=0; i<reserveSub.length; i++)
-    reserveSub[i].classList.toggle('active')
-})
-
-//-------------- 스크롤 탑버튼 -------------------------------------------------
-const elTop = document.querySelector('.topbtn');
+    reserveFix.addEventListener('click',function(){
+        reserveSub[i].classList.toggle('active')
+    })
+}
+//---------------- 스크롤 탑버튼 -------------------------------------------------
         
 window.addEventListener('scroll',function(){
-    if(window.innerHeight < window.scrollY + 700){
+    if(window.innerHeight < window.scrollY +700){
         elTop.classList.add('active');
     }else{
         elTop.classList.remove('active');
     };
 });
-const elCon = document.querySelectorAll('main');
-const elHeader = document.querySelector('header').offsetHeight;
+
 
 elTop.addEventListener('click',function(e){
     e.preventDefault();
 
     window.scrollTo({
-        top:elCon.offsetTop-elHeader,
+        top:elmain.offsetTop-elHeader,
         behavior:"smooth"
     });
 })
+//******************** function *******************************************//
+
+function sizeChange(){
+  for(let i=0; i<reserveSub.length; i++){
+
+      if(window.innerWidth < 1120 ) {
+          navul.classList.add('open')
+          if( !reserveSub[i].classList.contains('active')){
+              reserveSub[i].classList.add('active');
+          }
+      }else{
+          navul.classList.remove('open')
+          if( reserveSub[i].classList.contains('active')){
+              reserveSub[i].classList.remove('active');
+          }
+      }
+  }
+}
+sizeChange();
